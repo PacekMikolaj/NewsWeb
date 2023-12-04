d5a078cc6ce876434df384a8cacbb1726a83d93e
Mikołaj Pacek <mikolaj.pacek@bpower2.com>
2023-12-04 20:31:23 +0100

    added initial platform with react, typescript, webpack and less

---
+++ webpack.config.ts
@@ -0,0 +1,59 @@
+import path from "path";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+export const config: Configuration = {
+  mode:
+    (process.env.NODE_ENV as "production" | "development" | undefined) ??
+    "development",
+  entry: "./src/entrypoint.tsx",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+};
+
+export default config;
