e4a030bed989b6d39d07620537e2b0ead776176c
mikolaj.pacek <mikolaj.pacek@bpower2.com>
2023-12-13 11:10:36 +0100

    added downloading image

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDownloadURL, ref } from "firebase/storage";
+import { firebaseStorage } from "../../firebase";
 
 type NewsProps = {
   data: any;
 };
 
 const News: React.FC<NewsProps> = ({ data }) => {
+  const [url, setUrl ] = useState<string>("");
+
+  useEffect(()=> {
+    getDownloadURL(ref(firebaseStorage, `news_images/${data.image}`)).then((url: string) => {
+      console.log(url);
+      setUrl(url)
+    }).catch(err => console.log(err))
+
+
+  },[])
   console.log(data);
-  return <div>News</div>;
+  return <div>News
+    <img width="200px" src={url} alt={url}/>
+
+  </div>;
 };
 
 export default News;
